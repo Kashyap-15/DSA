6c033c60a683d31967588007f4f49b815702cddd
Kashyap <121273824+Kashyap-15@users.noreply.github.com>
2023-09-04 19:04:58 +0530

    Add files via upload

---
+++ DSA 2.0 By PW/Lecture1_Variables/Q5_Curcumference_circle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int radius = 10;
+    float PI = 3.14;
+    float circumference = 2 * PI * radius;
+    cout<<"Circumference of circle is : "<<circumference;
+    return 0;
+}
